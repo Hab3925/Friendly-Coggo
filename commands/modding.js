cf767d92c4a66d5e4c200598293f6af2208c0bfa
MelodicAlbuild <ralexdrum@gmail.com>
2021-05-04 12:25:58 -0500

    Update Modding cause WHY NOT

--- commands/modding.js
+++ commands/modding.js
@@ -1,64 +1,43 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
   const moddingJSON = require("../storage/modding.json");
-  let globalArray;
   if (!args[0]) {
     message.channel.send(
       `Learning to Mod? Check out the Modding Guide and FAQ! \nOur Modding Guide is at https://modding.melodicalbuild.me \nOur in Discord FAQ is found here! <#831103119252520960>`
     );
   } else {
-    // moddingJSON.data.forEach((element) => {
-    //   element.searchTerms.forEach((element2) => {
-    //     if (element2 == args[0].toLowerCase()) {
-    //       globalArray = moddingJSON.baseURL + element.url;
-    //     } else {
-    //       element.data.forEach((element3) => {
-    //         element3.searchTerms.forEach((element4) => {
-    //           if (element4 == args[0].toLowerCase()) {
-    //             globalArray = moddingJSON.baseURL + element3.url;
-    //           } else {
-    //             element3.data.forEach((element5) => {
-    //               element5.searchTerms.forEach((element6) => {
-    //                 if (element6 == args[0].toLowerCase()) {
-    //                   globalArray = moddingJSON.baseURL + element5.url;
-    //                 } else {
-    //                   if (globalArray == null) {
-    //                     globalArray =
-    //                       "Your Search Term was not found, Try again!";
-    //                   }
-    //                 }
-    //               });
-    //             });
-    //           }
-    //         });
-    //       });
-    //     }
-    //   });
-    // });
 
     var needle = args[0].toLowerCase();
     var foundEntries = []; // Whatever the JS version is.
 
     moddingJSON.data.forEach((dataEntry) => {
       dataEntry.searchTerms.forEach((haystackElement) => {
-        if (haystackElement == needle || haystackElement.includes(needle)) {
+        var lowercaseHaystack = haystackElement.toLowerCase();
+
+        if (lowercaseHaystack == needle || lowercaseHaystack.includes(needle)) {
           foundEntries.push(dataEntry);
         }
       });
     });
 
     let embed = new Discord.MessageEmbed()
+    .setTitle("Search Results:")
       .setTimestamp()
       .setFooter(
         "Friendly Coggo",
         client.user.avatarURL({ format: "png", size: 2048 })
       );
 
+    let count = 0;
+
     if (foundEntries.length > 0) {
-        embed.setColor("#0cc20f");
-      foundEntries.forEach(element => {
-          embed.addField("Search Results:", moddingJSON.baseURL + element.url);
-      })
+      embed.setColor("#0cc20f");
+      foundEntries.forEach((element) => {
+        if(count != 5) {
+            embed.addField(`${element.id}:`, moddingJSON.baseURL + element.url);
+            count++;
+        }
+      });
     } else {
       embed.setColor("#b80000");
       embed.addField(
@@ -66,14 +45,6 @@ module.exports.run = async (client, message, args) => {
         "Your Search Term was not found, Try again!"
       );
     }
-
-    // if (globalArray == "Your Search Term was not found, Try again!") {
-    //   embed.setColor("#b80000");
-    //   embed.addField("Search Results:", globalArray);
-    // } else {
-    //   embed.setColor("#0cc20f");
-    //   embed.addField("Search Results:", globalArray);
-    // }
 
     message.channel.send(embed);
   }
